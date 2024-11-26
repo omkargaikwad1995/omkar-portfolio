2018e16aaf437aa696aaaef1321dc51942cde7be
omkargaikwad1995 <omkargaikwad1221@gmail.com>
2024-11-26 22:09:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import { About, Hero, Contact, Experience, Feedbacks, StarsCanvas, Tech, Works } from './components'
+
+function App() {
+  
+
+  return (
+    <>
+    <BrowserRouter>
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar />
+        <Hero/>
+      </div>
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+      <div className='relative z-0'>
+        <Contact/>
+        <StarsCanvas/>
+      </div>
+    </div>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
